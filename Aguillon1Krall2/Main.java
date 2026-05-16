1887e4065717e1abe5943582b516e7fffe6332f3
Franz Aguillon <franzaguillon@gmail.com>
2026-05-16 18:20:55 +0200

    Implement methods for Main.java

---
+++ Aguillon1Krall2/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public void convertToPDF(File file) {
+
+    }
+
+    public void convertToXML(File file) {
+
+    }
+}
