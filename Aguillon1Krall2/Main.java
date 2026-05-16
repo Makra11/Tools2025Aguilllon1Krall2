8ce9087d0d5f7a4fedc89d25a054430cc312979d
Martin Krall <s2510237020@fhooe.at>
2026-05-16 20:36:11 +0200

    Add convertToXML method to Main.java

--- Aguillon1Krall2/Main.java
+++ Aguillon1Krall2/Main.java
@@ -8,7 +8,7 @@ public class Main {
     }
 
     public void convertToXML(File file) {
-
+	System.out.println("XML-File has been successfully converted");
     }
 
     public void cleanFile(File file) {
